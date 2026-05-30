cd6802a601068a953392d66665e7580856a48595
Paulo Roberto A. Pinheiro <58056132+aguirre39@users.noreply.github.com>
2026-05-30 14:41:43 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./*.html", "./**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        brand: {
+          magenta: '#ff007f', 
+          'magenta-dark': '#be005e',
+          'magenta-light': '#ff3399',
+          gray: '#2b2630', 
+          'gray-dark': '#17141a',
+          'gray-darker': '#0c0a0d', 
+          'gray-light': '#7a7e8a',
+        }
+      }
+    }
+  }
+}
